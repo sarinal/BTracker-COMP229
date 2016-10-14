b65d479dd892ef7b2dfb49993a9db46ea6165efa
Sam <Sam@DESKTOP-O0S9A91>
2016-10-14 17:52:08 -0400

    Added Team, date selection for inputpage

---
+++ BTracker-COMP229/InputStats.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Input Stats" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InputStats.aspx.cs" Inherits="BTracker_COMP229.InputStats" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col md 6">
+                
+                <h6>Home Team</h6>
+                <asp:Button Text="American" ID="HomeALButton" CssClass="btn btn-primary btn-md" runat="server" onclick="HomeALList_Click" />
+                <asp:Button Text="National" ID="HomeNLButton" CssClass="btn btn-warning btn-md" runat="server" onclick="HomeNLList_Click" />
+                
+                <asp:DropDownList ID="TeamHomeList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="TeamHomeList_SelectedIndexChanged">
+                </asp:DropDownList>
+                <asp:Image ID="HomeImage" runat="server" ImageUrl="~/Assets/images/baseball_logo_sm.png"/>
+
+
+                <label class="control-label" for="GameDateTextbox">Date </label>
+                    <asp:TextBox runat="server" ID="GameDateTextbox" 
+                        placeholder="Enrollment Date" requiered="true" TextMode="Date"></asp:TextBox>
+
+                <h6>Away Team</h6>
+                <asp:Button Text="American" ID="AwayALButton" CssClass="btn btn-primary btn-md" runat="server" onclick="AwayALList_Click" />
+                <asp:Button Text="National" ID="AwayNLButton" CssClass="btn btn-warning btn-md" runat="server" onclick="AwayNLList_Click" />
+
+                <asp:DropDownList ID="TeamAwayList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="TeamAwayList_SelectedIndexChanged">
+
+                </asp:DropDownList>
+                <asp:Image ID="AwayImage" runat="server" ImageUrl="~/Assets/images/baseball_logo_sm.png" />
+
+
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ BTracker-COMP229/InputStats.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTracker_COMP229
+{
+    public partial class InputStats : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void HomeALList_Click(object sender, EventArgs e)
+        {
+            TeamHomeList.Items.Clear();
+                TeamHomeList.Items.Add(new ListItem("Baltimore Orioles", "AL1"));
+                TeamHomeList.Items.Add(new ListItem("Boston Red Sox", "AL2"));
+            
+        }
+
+        protected void HomeNLList_Click(object sender, EventArgs e)
+        {
+            TeamHomeList.Items.Clear();
+            TeamHomeList.Items.Add(new ListItem("Arizona Diamondbacks", "NL1"));
+            TeamHomeList.Items.Add(new ListItem("Atlanta Braves", "NL2"));
+
+        }
+
+        protected void AwayALList_Click(object sender, EventArgs e)
+        {
+            TeamAwayList.Items.Clear();
+            TeamAwayList.Items.Add(new ListItem("Baltimore Orioles", "AL1"));
+            TeamAwayList.Items.Add(new ListItem("Boston Red Sox", "AL2"));
+
+        }
+
+        protected void AwayNLList_Click(object sender, EventArgs e)
+        {
+            TeamAwayList.Items.Clear();
+            TeamAwayList.Items.Add(new ListItem("Arizona Diamondbacks", "NL1"));
+            TeamAwayList.Items.Add(new ListItem("Atlanta Braves", "NL2"));
+
+        }
+
+        protected void TeamAwayList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Team = TeamAwayList.SelectedValue;
+            AwayImage.ImageUrl = "~/Assets/images/" + Team + ".jpg";
+        }
+
+        protected void TeamHomeList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Team = TeamHomeList.SelectedValue;
+            HomeImage.ImageUrl = "~/Assets/images/" + Team + ".jpg";
+        }
+    }
+}
